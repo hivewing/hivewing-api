ebabdaf0e3966c0a4beb0e4a70462bca918a4f59
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2014-11-18 13:48:38 -0500

    updated to have the first hive API endpoint!

--- src/hivewing_api/handler.clj
+++ src/hivewing_api/handler.clj
@@ -1,11 +1,12 @@
 (ns hivewing-api.handler
   (:require [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
-            [hivewing-api.hives :refer [hives-api-routes]]
+            [hivewing-api.hives :refer :all]
             [schema.core :as s]))
 
 (defapi app
   (swagger-ui)
+
   (swagger-docs "/api/api-docs"
     :title "Hivewing-api"
     :apiVersion "0.1")
@@ -16,4 +17,5 @@
 
   (swaggered "workers"
     :description "Workers API"
-    ))
+    )
+)
--- src/hivewing_api/hives.clj
+++ src/hivewing_api/hives.clj
@@ -1,26 +1,40 @@
 (ns hivewing-api.hives
-  (:require [compojure.api.sweet :as sweet]
-            [ring.util.http-response :as response]
-            [ring.swagger.schema :as ss]
+  (:require [compojure.api.sweet :refer :all]
             [hivewing-core.hive :as hive-core]
-            [ring.middleware.basic-authentication :refer [wrap-basic-authentication]]
-            [schema.core :as schema]))
+            [ring.util.http-response :refer :all]
+            [ring.swagger.schema :refer [describe]]
+            [schema.core :as s]))
 
-(schema/defschema Hive {:name String,
-                        :worker-count Long})
+(s/defschema Hive {:name String,
+                   :uuid java.util.UUID,
+                   :updated_at (s/maybe java.sql.Timestamp)
+                   :created_at java.sql.Timestamp,
+                   :apiary_uuid java.util.UUID})
 
-(defn hive-auth? [hive-uuid access-token]
-  "Look up the hiveuuid and access token, see if it matches.
-   if so return the data (and role information)"
-  {:uuid "123"
-   :roles []})
+(defmethod compojure.api.meta/restructure-param :hive-access
+  [_ hive-permissions {:keys [parameters lets body middlewares] :as acc}]
+  "Make sure the request has X-Hive-Access-Token header and that it's poiting to an access token
+   that is owned by the hive pointed to by hive-uuid (path_param)"
+  (let [let-ds `[{{raw-token# "x-hive-access-token"} :headers} ~'+compojure-api-request+
+                 hive-uuid# (:hive-uuid (:params ~'+compojure-api-request+))
+                 ~hive-permissions (hivewing-core.hive/hive-get-permissions hive-uuid# raw-token#)]]
+    (-> acc
+      (update-in [:lets] into let-ds)
+      (assoc :body `((if ~hive-permissions
+                     (do
+                       ~@body)
+                     (ring.util.http-response/forbidden "Auth required")))))))
 
 ; The hives API routes
-(def hives-api-routes
-  (wrap-basic-authentication hive-auth?
-    (sweet/context "/hives" {hive-auth :basic-authentication, :as request}
-      (sweet/GET* "/" []
-        :summary "Get all the data about this hive"
-        :return Hive
-        (response/ok {:name "Worker" :worker-count 1})
-    ))))
+(defroutes* hives-api-routes
+  (context "/hives/:hive-uuid" []
+    (GET* "/" []
+      :path-params [hive-uuid :- (ring.swagger.schema/describe String "Uuid of the hive")]
+      :summary "Get all the data about this hive"
+      :hive-access permissions
+      :return Hive
+      (let [hive (hivewing-core.hive/hive-get hive-uuid)]
+        (if hive
+          (ok hive)
+          (not-found (str "Could not find the hive " hive-uuid))))
+  )))
