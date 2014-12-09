2bc0ecfb8bf902dfa348cf8c24992d7c41e98530
gitolite tester <tester@example.com>
2014-12-09 12:32:50 -0500

    update version and fix uberjarring

--- project.clj
+++ project.clj
@@ -5,12 +5,11 @@
                  [metosin/ring-http-response "0.5.2"]
                  [metosin/ring-swagger-ui "2.0.17"]
                  [environ "1.0.0"]
-                 [ring-basic-authentication "1.0.5"]
-                 [hivewing-core "0.1.1"]]
+                 [hivewing-core "0.1.2"]]
 
   :ring {:handler hivewing-api.handler/app}
 
-  :uberjar-name "hivewing-api_%s.jar"
+  :uberjar-name "hivewing-api-%s.uber.jar"
 
   :repositories [["hivewing-core" {:url "s3p://clojars.hivewing.io/hivewing-core/releases"
                                    :username "AKIAJCSUM5ZFGI7DW5PA"
@@ -19,15 +18,7 @@
   :plugins [[lein-ring "0.8.13"]
             [s3-wagon-private "1.1.2"]
             [lein-environ "1.0.0"]
-            [lein-ring "0.8.13"]
             ]
 
   :profiles {:uberjar {:resource-paths ["swagger-ui"]}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}})
-
-  ;   :hivewing-redis-uri "redis://127.0.0.1:3900/"
-  ;   :hivewing-aws-access-key "123abc",
-  ;   :hivewing-aws-secret-key "123abc",
-  ;   :hivewing-ddb-endpoint   "http://127.0.0.1:3800",
-  ;   :hivewing-ddb-worker-config-table "HivewingWorkerConfiguration.v1"
-  ;   :hivewing-sql-connection-string "//127.0.0.1:5432/hivewing-dev?user=hivewing&password=hivewing"
